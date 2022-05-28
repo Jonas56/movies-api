b566fbdd38c0eb4abd5d05075211926d09fdc91a
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-05-28 19:20:44 +0100

    Implement Movie entity to DTO conversion

---
+++ src/main/java/com/marsox/movies/dto/MovieImagesDto.java
@@ -0,0 +1,12 @@
+package com.marsox.movies.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor(staticName = "build")
+@NoArgsConstructor
+public class MovieImagesDto {
+    private String link;
+}
--- src/main/java/com/marsox/movies/service/MovieService.java
+++ src/main/java/com/marsox/movies/service/MovieService.java
@@ -1,8 +1,10 @@
 package com.marsox.movies.service;
 
+import com.marsox.movies.dto.*;
 import com.marsox.movies.model.Actor;
 import com.marsox.movies.model.Director;
 import com.marsox.movies.model.Movie;
+import com.marsox.movies.model.MovieImage;
 import com.marsox.movies.repository.ActorRepository;
 import com.marsox.movies.repository.DirectorRepository;
 import com.marsox.movies.repository.MovieRepository;
@@ -12,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MovieService {
@@ -26,11 +29,12 @@ public class MovieService {
         this.directorRepository = directorRepository;
     }
 
-    public List<Movie> getAllMovies() {
-        return movieRepository.findAll();
+    public List<MovieEssentialDto> getAllMovies() {
+        return movieRepository.findAll().stream()
+                .map(this::convertEntityToMovieEssentialsDto).collect(Collectors.toList());
     }
 
-    public Movie addNewMovie(Movie m) {
+    public MovieDto addNewMovie(Movie m) {
         for (int i = 0; i < m.getActors().size(); i++) {
             Actor actor = m.getActors().get(i);
             Optional<Actor> actorExist =
@@ -46,7 +50,7 @@ public class MovieService {
         directorRepository.findByFullName(director.getFullName())
                 .ifPresent(m::setDirector);
 
-        return movieRepository.save(m);
+        return convertEntityToMovieDto(movieRepository.save(m));
     }
 
     public void deleteMovie(Long movieId) {
@@ -60,17 +64,17 @@ public class MovieService {
         movieRepository.deleteById(movieId);
     }
 
-    public List<Movie> getMoviesByName(String movieName) {
+    public List<MovieEssentialDto> getMoviesByName(String movieName) {
         return movieRepository
                 .findByName(movieName)
                 .orElseThrow(() ->
                         new ResponseStatusException(HttpStatus.BAD_REQUEST,
                                 "Movie with name : " + movieName + " not found!"
                         )
-                );
+                ).stream().map(this::convertEntityToMovieEssentialsDto).collect(Collectors.toList());
     }
 
-    public Movie updateMovie(Long movieId, Movie m) {
+    public MovieDto updateMovie(Long movieId, Movie m) {
         boolean exists = movieRepository.existsById(movieId);
 
         if (!exists) {
@@ -80,14 +84,44 @@ public class MovieService {
             );
         }
 
-        return movieRepository.save(m);
+        return convertEntityToMovieDto(movieRepository.save(m));
     }
 
-    public Movie getMovieById(Long id){
-        return movieRepository.findById(id).orElseThrow(()->
-            new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                    "Movie with id : " + id + " not found!"
-            )
+    public MovieDto getMovieById(Long id) {
+        return convertEntityToMovieDto(movieRepository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                        "Movie with id : " + id + " not found!"
+                )
+        ));
+    }
+
+    private MovieEssentialDto convertEntityToMovieEssentialsDto(Movie movie) {
+        return MovieEssentialDto.build(
+                movie.getName(), movie.getImdbRating(),
+                movie.getTrailerLink(), movie.getGenre(),
+                movie.getMovieImages().stream().map(this::convertEntityToMovieImagesDto).collect(Collectors.toSet())
+        );
+    }
+
+    private MovieImagesDto convertEntityToMovieImagesDto(MovieImage movieImage) {
+        return MovieImagesDto.build(movieImage.getLink());
+    }
+
+    private ActorDto convertEntityToActorDto(Actor actor) {
+        return ActorDto.build(actor.getFullName(), actor.getBornDate());
+    }
+
+    private DirectorDto convertEntityToDirectorDto(Director director) {
+        return DirectorDto.build(director.getFullName(), director.getBornDate());
+    }
+
+    private MovieDto convertEntityToMovieDto(Movie movie) {
+        return MovieDto.build(
+                movie.getId(), movie.getName(), movie.getDescription(), movie.getImdbRating(), movie.getTrailerLink(),
+                movie.getReleaseDate(), movie.getGenre(), movie.getBudget(),
+                movie.getMovieImages().stream().map(this::convertEntityToMovieImagesDto).collect(Collectors.toSet()),
+                movie.getActors().stream().map(this::convertEntityToActorDto).collect(Collectors.toList()),
+                this.convertEntityToDirectorDto(movie.getDirector())
         );
     }
 }
