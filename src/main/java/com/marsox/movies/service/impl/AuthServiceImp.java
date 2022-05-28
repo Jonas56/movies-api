5aba00718af27468fcfd93d231a1a7267a0b4a97
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-05-28 20:20:31 +0100

    Validate username unique constraints

--- src/main/java/com/marsox/movies/service/impl/AuthServiceImp.java
+++ src/main/java/com/marsox/movies/service/impl/AuthServiceImp.java
@@ -4,8 +4,10 @@ import com.marsox.movies.dto.UserDto;
 import com.marsox.movies.model.User;
 import com.marsox.movies.repository.UserRepository;
 import com.marsox.movies.service.IAuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class AuthServiceImp implements IAuthService {
@@ -19,6 +21,10 @@ public class AuthServiceImp implements IAuthService {
     }
 
     public UserDto addNewUser(User user) {
+        boolean userExists = userRepository.existsByUsernameOrEmail(user.getUsername(), user.getEmail());
+        if (userExists){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User already exists");
+        }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return convertEntityToDto(userRepository.save(user));
     }
