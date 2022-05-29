1108db535fdf6693e70efb0e973ac34cbdf14b26
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-05-29 01:02:38 +0100

    Update ApplicationErrorHandler

--- src/main/java/com/marsox/movies/errors/ApplicationErrorHandler.java
+++ src/main/java/com/marsox/movies/errors/ApplicationErrorHandler.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
