5460df6a3d4327753acae8ffc5f56050bf00b5a3
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-05-29 03:29:50 +0100

    OpenApi configuration

---
+++ src/main/java/com/marsox/movies/config/OpenApi30Config.java
@@ -0,0 +1,24 @@
+package com.marsox.movies.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Movies API",
+                version = "v1",
+                description = "Spring REST API where you can explore and save your favorite movies"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApi30Config {
+}
